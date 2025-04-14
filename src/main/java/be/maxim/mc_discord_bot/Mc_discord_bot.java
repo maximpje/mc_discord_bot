534e881912d3b3e7ed54b629d7e1d5e208942ceb
Maxim Wynant <wynantmaxim@gmail.com>
2025-04-14 22:58:40 +0200

    initial commit

---
+++ src/main/java/be/maxim/mc_discord_bot/Mc_discord_bot.java
@@ -0,0 +1,17 @@
+package be.maxim.mc_discord_bot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mc_discord_bot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
