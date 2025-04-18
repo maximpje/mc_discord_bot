0a1002e1d743042591ac37e153bba014eb2b2af8
Maxim Wynant <wynantmaxim@gmail.com>
2025-04-18 21:31:07 +0200

    add discord object in plugin

--- src/main/java/be/maxim/mc_discord_bot/McDiscordBot.java
+++ src/main/java/be/maxim/mc_discord_bot/McDiscordBot.java
@@ -1,6 +1,8 @@
 package be.maxim.mc_discord_bot;
 
 import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.security.auth.login.LoginException;
 import java.util.Objects;
 
 
@@ -10,7 +12,13 @@ public final class McDiscordBot extends JavaPlugin {
     public void onEnable() {
         System.out.println("Mc_discord_bot is starting...");
         Objects.requireNonNull(this.getCommand("test")).setExecutor(new CommandTest());
-
+        DiscordClient bot = null;
+        try {
+            bot = new DiscordClient();
+        } catch (LoginException e) {
+            throw new RuntimeException(e);
+        }
+        bot.StartBot();
     }
 
     @Override
