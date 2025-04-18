72dc7cf199f122b6ddc467fdaf342ff6b677ea50
Maxim Wynant <wynantmaxim@gmail.com>
2025-04-18 15:21:16 +0200

    add jda to pom.xml and initialised in main class

--- src/main/java/be/maxim/mc_discord_bot/Mc_discord_bot.java
+++ src/main/java/be/maxim/mc_discord_bot/Mc_discord_bot.java
@@ -1,17 +1,39 @@
 package be.maxim.mc_discord_bot;
 
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.security.auth.login.LoginException;
 import java.util.Objects;
-import java.io.File;
-import java.util.Scanner;
+//import java.io.File;
+//import java.util.Scanner;
 
 public final class Mc_discord_bot extends JavaPlugin {
+
+    private final ShardManager shardManager;
+
+    public Mc_discord_bot() throws LoginException {
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault("TOKEN");
+        builder.setStatus(OnlineStatus.ONLINE);
+        shardManager = builder.build();
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
 
     @Override
     public void onEnable() {
         System.out.println("Mc_discord_bot is starting...");
         Objects.requireNonNull(this.getCommand("test")).setExecutor(new CommandTest());
 
+        try {
+            Mc_discord_bot bot = new Mc_discord_bot();
+        } catch (LoginException e) {
+            System.out.println("ERROR: Discord ogin exception");
+        }
     }
 
     @Override
