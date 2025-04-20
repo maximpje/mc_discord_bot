df685e736720ba2274983290588dc7d62e46a687
Maxim Wynant <wynantmaxim@gmail.com>
2025-04-20 22:44:25 +0200

    FEAT: add player join event and discord message

--- src/main/java/io/github/maximpje/mc_discord_bot/DiscordEventListener.java
+++ src/main/java/io/github/maximpje/mc_discord_bot/DiscordEventListener.java
@@ -1,18 +1,18 @@
 package io.github.maximpje.mc_discord_bot;
 
-import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
 
 public class DiscordEventListener extends ListenerAdapter {
 
-    public Channel channel;
+    public static MessageChannel channel;
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (event.getName().equals("connect")){
-            channel = event.getChannel();
+            channel = event.getMessageChannel();
             event.reply("Set Broadcast channel").queue();
 
             Bukkit.getServer().broadcastMessage("Broadcast channel in discord set to: " + channel.getName());
--- src/main/java/io/github/maximpje/mc_discord_bot/MinecraftEventListener.java
+++ src/main/java/io/github/maximpje/mc_discord_bot/MinecraftEventListener.java
@@ -1,7 +1,18 @@
 package io.github.maximpje.mc_discord_bot;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import static io.github.maximpje.mc_discord_bot.DiscordEventListener.channel;
 
 public class MinecraftEventListener implements Listener{
-
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        try {
+            channel.sendMessage("Player joined the server: " + event.getPlayer().getName()).queue();
+        }catch(java.lang.NullPointerException){
+            System.out.println("EXCEPT: Discord channel not set!");
+        }
+    }
 }
