0e700d86028c86c08e61be20f0aa58d242473b08
Maxim Wynant <wynantmaxim@gmail.com>
2025-04-21 12:00:41 +0200

    FEAT: add advancement event/message

--- src/main/java/io/github/maximpje/mc_discord_bot/MinecraftEventListener.java
+++ src/main/java/io/github/maximpje/mc_discord_bot/MinecraftEventListener.java
@@ -1,8 +1,12 @@
 package io.github.maximpje.mc_discord_bot;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.Objects;
 
 import static io.github.maximpje.mc_discord_bot.DiscordEventListener.channel;
 
@@ -10,7 +14,18 @@ public class MinecraftEventListener implements Listener{
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         try {
-            channel.sendMessage("Player joined the server: " + event.getPlayer().getName()).queue();
+            String playerName = event.getPlayer().getName();
+            channel.sendMessage(playerName + ": Joined the server!").queue();
+            Bukkit.broadcastMessage("Welcome to the server " + playerName + "!");
+        }catch(NullPointerException e){
+            System.out.println("EXCEPT: Discord channel not set!");
+        }
+    }
+    @EventHandler
+    public void onPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
+        try {
+            if(!event.getAdvancement().getKey().toString().contains("recipe"))
+               channel.sendMessage(event.getPlayer().getName() + " has made " + Objects.requireNonNull(event.getAdvancement().getDisplay()).getTitle() + " congratulations!").queue();
         }catch(NullPointerException e){
             System.out.println("EXCEPT: Discord channel not set!");
         }
